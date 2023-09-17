7896675229352ba2c39b58c64f861cdfd91e02f1
Snack Man <lpardo1989@gmail.com>
2023-09-17 12:28:48 -0700

    update to Homepage and Layout

---
+++ src/components/Home/index.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import '../Home/index.scss';
+
+
+const Home = () => {
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm
+                    <img src={LogoTitle} alt="developer" />
+                    nack_mann
+                    <br />
+                    your friendly neighborhood web developer
+                </h1>
+                <h2>Frontend Developer / javascript expert</h2>
+                <Link to="/contact" className="flat-button">Contact me</Link>
+            </div>
+        </div>
+    );
+}
+
+
+export default Home
