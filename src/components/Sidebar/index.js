a0ee842a3e78dca8dc4c0516c2e47c776533e187
Snack Man <lpardo1989@gmail.com>
2023-09-16 21:25:28 -0700

    update to css for sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import { Link, NavLink } from 'react-router-dom';
-import './index.scss';
+import '../Sidebar/index.scss';
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -15,17 +15,21 @@ const Sidebar = () => (
             <img src={LogoS} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="LP" />
         </Link>
-        <NavLink exact="true" activeclassname="active" to="/">
+        <nav>
+        <NavLink exact="true" activeClassName="active" to="/">
+
             <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
         </NavLink>
-        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+        <NavLink exact="true" activeClassName="active" className="about-link" to="/about">
             <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
         </NavLink>
-        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+        <NavLink exact="true" activeClassName="active" className="contact-link" to="/contact">
             <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
-    </div>    
+        </nav>
+    </div>      
 )
 
 
 export default Sidebar;
+
