437522951a592c08e9f03cc4de37796391427370
Snack Man <lpardo1989@gmail.com>
2023-09-18 11:00:19 -0700

    Projects has new icons, updated CSS for Projects page.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,37 +5,37 @@ const Projects = () => {
   const projects = [
     {
       title: "Tech Blog",
-      image: "github3.png",
+      image: "code.png",
       deployedLink: "https://project1.com",
       githubLink: "https://github.com/kingcaliente/Tech-Blog"
     },
     {
       title: "Quiz App",
-      image: "github3.png",
+      image: "coding.png",
       deployedLink: "https://kingcaliente.github.io/quiz-game/",
       githubLink: "https://github.com/kingcaliente/quiz-game"
     },
     {
       title: "Text Editor",
-      image: "github3.png",
+      image: "github5.png",
       deployedLink: "https://github.com/kingcaliente/mod19",
       githubLink: "https://github.com/kingcaliente/mod19"
     },
     {
-      title: "Stream App",
-      image: "github3.png",
+      title: "Streaming Availability App",
+      image: "backend.png",
       deployedLink: "https://project4.com",
       githubLink: ""
     },
     {
       title: "Read Me Generator",
-      image: "github3.png",
+      image: "layers.png",
       deployedLink: "https://github.com/kingcaliente/README-generator",
       githubLink: "https://github.com/kingcaliente/README-generator"
     },
     {
       title: "Weather Dashboard",
-      image: "github3.png",
+      image: "world-wide-web.png",
       deployedLink: "https://github.com/kingcaliente/weather-dashboard",
       githubLink: "https://github.com/kingcaliente/weather-dashboard"
     }
@@ -45,19 +45,19 @@ const Projects = () => {
     <div className="container about-page">
       <div className="text-zone">
         <h1>Portfolio Of Work</h1>
-        {projects.map((project, index) => (
-          <div key={index}>
-            <h2>{project.title}</h2>
-            <img src={project.image} alt={project.title} />
-            <a href={project.deployedLink}>Deployed App</a>
-            <a href={project.githubLink}>GitHub Repository</a>
-          </div>
-        ))}
+        <div className="projects-grid">
+          {projects.map((project, index) => (
+            <div key={index}>
+              <h2>{project.title}</h2>
+              <img src={project.image} alt={project.title} />
+              <a href={project.deployedLink}>Deployed App</a>
+              <a href={project.githubLink}>GitHub Repository</a>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 }
 
-
-
-      export default Projects;
+export default Projects;
