2a33e4fde5eb12cbd52b4c59d5e7ab57956e750d
Snack Man <lpardo1989@gmail.com>
2023-09-17 18:10:22 -0700

    adding projects to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,16 @@
 import './index.scss';
-
+import LP from '../../assets/images/projects.png';
 const Contact = () => {
-  return (
-    <div className="contact">
-      <h1>Contact</h1>
-    </div>
-  );
-};
+    return (
+        <div className="app">
+            <h3>quiz game</h3>
+            <a href="https://github.com/kingcaliente/quiz-game">
+                <img src={LP} alt="LP-Portfolio" />
+            </a>
+            <a href="https://github.com/kingcaliente/quiz-game">View on GitHub</a>
+        </div>
+    );
+}
 
 export default Contact;
 
